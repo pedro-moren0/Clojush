4ec8d93473b625c720abba31a497ab5984840e42
pedro-moren0 <pedro.moreno@aluno.ufabc.edu.br>
2025-01-09 16:09:12 -0300

    melhorias no script dos resultados

--- run_pgc.sh
+++ run_pgc.sh
@@ -15,14 +15,20 @@ bms=(
 # "word-stats"
 )
 
-for b in ${bms[@]};
-do
-	for i in {1..100};
-	do
-		echo "Running $b $i/100"
-		out_file="$b-$i.out"
-		lein run clojush.problems.software."$b" > "data/$out_file"
-	done
-done
+if [ $# != 1 ]; then
+	echo "Invalid number of arguments!"
+	echo "Usage: ./run_pgc.sh [WORK_DIR]"
+else
+	work_dir=$1
+	output_folder="$work_dir/data"
+	mkdir -p "$output_folder"
 
-# lein run clojush.problems.software.syllables > syllables.out
+	for b in ${bms[@]};
+	do
+		for i in {1..100};
+		do
+			out_file="$b-$i.out"
+			lein run clojush.problems.software."$b" > "$output_folder/$out_file"
+		done
+	done
+fi
